d6cf8ae8695c0188970e93c72855828fa6f25d01
visualstudio <visualstudio@122-PC14>
2019-04-05 20:22:27 -0400

    Section B

--- C0724144-Assignment4/Program.cs
+++ C0724144-Assignment4/Program.cs
@@ -14,13 +14,15 @@ namespace C0724144_Assignment4
     class Program
     {
         ArrayList Beowulf;
+        private char[] separator;
 
         static void Main(string[] args)
         {
             Program p = new Program();
-            p.Beowulf = new ArrayList();
+           
             p.ReadTextFiles();
-
+            p.Beowulf = new ArrayList();
+            p.TotalWords();
             Console.ReadLine();
         }
         public void Run()
@@ -32,26 +34,21 @@ namespace C0724144_Assignment4
         {
             // Read file using StreamReader. Read file line by line
             using (StreamReader file = new StreamReader("Beowulf.txt"))
+
             {
 
                 int counter = 0;
                 string ln;
-
+               string words;
                 while ((ln = file.ReadLine()) != null)
                 {
                     Console.WriteLine(ln);
-                    Beowulf.Add(ln);
                 }
                 file.Close();
-
                 counter = File.ReadLines("Beowulf.txt").Count();
-
                 Console.WriteLine("\n\n\nThe file has "+counter+" lines");
-
-
-                
+                }
             }
-        }
         public int FindNumberOfBlankSpaces(string line)
         {
             https://stackoverflow.com/questions/17812566/count-words-and-spaces-in-string-c-sharp
@@ -66,6 +63,35 @@ namespace C0724144_Assignment4
             }
             return countSpaces;
 
+        }
+
+
+        public long TotalWords()
+        {
+
+            StreamReader reader = new StreamReader("Beowulf.txt");
+            string script = reader.ReadToEnd();
+
+            var text = script.Trim();
+            long wordCount = 0; 
+             int index = 0;
+
+            while (index < text.Length)
+            {
+                // check if current char is part of a word
+                while (index < text.Length && !char.IsWhiteSpace(text[index]))
+                    index++;
+
+                wordCount++;
+
+                // skip whitespace until next word
+                while (index < text.Length && char.IsWhiteSpace(text[index]))
+                    index++;
+            }
+
+            Console.WriteLine("The file has "+wordCount);
+            return wordCount;
+            
         }
 
     }
